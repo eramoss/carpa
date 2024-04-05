d004014d91ad11b42b5db12c8f02fcdc7ea3d4d6
eramoss <edulramos@outlook.com>
2024-04-05 11:14:40 -0300

    trying to create a gen server

--- lib/carpa.ex
+++ lib/carpa.ex
@@ -1,18 +1,20 @@
 defmodule Carpa do
-  @moduledoc """
-  Documentation for `Carpa`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Carpa.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  use GenServer
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
+
+  def init(init_arg) do
+    {:ok, init_arg}
+  end
+
+  def check_repo(repo, branch) do
+    GenServer.call(__MODULE__, {:check_repo, repo, branch})
+  end
+
+  def handle_call({:check_repo, repo, branch}, _from, state) do
+    IO.puts "Checking #{repo} on branch #{branch}"
+    {:reply, :ok, state}
+  end
+
 end
